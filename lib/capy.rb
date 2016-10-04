ba25950dc79aef3fbca8939e8c684724993499ab
navin <navin@amagi.com>
2016-10-05 00:59:21 +0530

    require files when loading capy

--- lib/capy.rb
+++ lib/capy.rb
@@ -1,5 +1,10 @@
 require "capy/version"
+require 'capy/base'
+require 'capy/list'
+require 'capy/util'
+require 'capy/cue'
+require 'capy/formats/srt'
 
-module Capy
-  # Your code goes here...
-end
+project_root = File.dirname(File.absolute_path(__FILE__))
+Dir.glob(project_root + '/formats/*') {|file| require file}
+
