cc5d6bb6b6d8be73e48d8a504a98cd044801a13a
navin <navin@amagi.com>
2016-10-09 22:44:28 +0530

    require CapyError classes

--- lib/capy.rb
+++ lib/capy.rb
@@ -1,9 +1,10 @@
-require "capy/version"
+require 'capy/version'
+require 'capy/error'
 require 'capy/util'
 require 'capy/base'
 require 'capy/list'
 require 'capy/cue'
 require 'capy/formats/srt'
-require "capy/formats/vtt"
+require 'capy/formats/vtt'
 
 require 'capy/export'
