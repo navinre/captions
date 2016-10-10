af7691c74ad9c6db601a94f540f5dc5d5ed38a5d
navin <navin@amagi.com>
2016-10-10 11:37:04 +0530

    add comments for Capy::Error

--- lib/capy/error.rb
+++ lib/capy/error.rb
@@ -1,17 +1,26 @@
 module Capy
 
+  # Used for all execptions thrown from Capy
   class CapyError < StandardError
   end
 
+  # Subtitle has to be in correct format
+  # when its being serialized
   class InvalidSubtitle < CapyError
   end
 
+  # Subtitle has to be correct format
+  # when its being parsed
   class MalformedString < CapyError
   end
 
+  # File has to be specifed for parsing
+  # subtitle
   class UnknownFile < CapyError
   end
 
+  # Input has to be proper for filtering
+  # subtitles
   class InvalidInput < CapyError
   end
 
