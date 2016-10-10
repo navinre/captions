a2ea752dcd58a4a758b872feb4639c1ccc91fac2
navin <navin@amagi.com>
2016-10-10 09:50:09 +0530

    fix indendation

--- lib/capy/error.rb
+++ lib/capy/error.rb
@@ -1,18 +1,18 @@
 module Capy
 
-	class CapyError < StandardError
-	end
+  class CapyError < StandardError
+  end
 
-	class InvalidSubtitle < CapyError
-	end
+  class InvalidSubtitle < CapyError
+  end
 
-	class MalformedString < CapyError
-	end
+  class MalformedString < CapyError
+  end
 
-	class UnknownFile < CapyError
-	end
+  class UnknownFile < CapyError
+  end
 
-	class InvalidInput < CapyError
-	end
+  class InvalidInput < CapyError
+  end
 
 end
