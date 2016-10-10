ae12890065760998cbe1e0147ff7de6bc42c6103
navin <navin@amagi.com>
2016-10-10 12:48:59 +0530

    add comments for Capy::VTT

--- lib/capy/formats/vtt.rb
+++ lib/capy/formats/vtt.rb
@@ -1,8 +1,10 @@
 module Capy
 	class VTT < Base
 
+    # Header used for all VTT files
 		VTT_HEADER = "WEBVTT"
 
+    # Parse VTT file and update CueList
 		def parse
 			base_parser do
 				count = 1
@@ -26,6 +28,7 @@ module Capy
 			end
 		end
 
+    # Export CueList to VTT file
 		def dump(file)
 			base_dump(file) do |file|
 				file.write(VTT_HEADER)
@@ -40,15 +43,18 @@ module Capy
 			end
 		end
 
+    # Timecode format used in VTT file
     def is_time?(text)
       !!text.match(/^\d{2}:\d{2}:\d{2}.\d{3}.*\d{2}:\d{2}:\d{2}.\d{3}/)
     end
 
+    # Get start-time and end-time
     def get_time(line)
       data = line.split('-->')
       return data[0], data[1]
     end
 
+    # Check whether if its subtilte text or not
     def is_text?(text)
       !text.empty? and text.is_a?(String) and text != VTT_HEADER
     end
