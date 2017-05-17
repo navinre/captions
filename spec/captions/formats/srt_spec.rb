eaae37091d9cd8de5dd0e89a3a11149be4966a09
navin <navin@amagi.com>
2017-05-17 20:14:13 +0530

    test(SRT): added more specs for SRT parsing

--- spec/captions/formats/srt_spec.rb
+++ spec/captions/formats/srt_spec.rb
@@ -12,5 +12,39 @@ module Captions
       expect(c.parse).to be true
     end
 
+    context "validate subtitle" do
+
+      before :each do
+        @subs = Captions::SRT.new(@sample_file)
+        @subs.parse
+      end
+
+      it "has correct number of cues" do
+        expect(@subs.cues.count).to eq 2
+      end
+
+      it "has correct start_time" do
+        correct_values = [1600,2767]
+        expect(@subs.cues.map{ |c| c.start_time}).to eq correct_values
+      end
+
+      it "has correct end_time" do
+        correct_values = [2684,6021]
+        expect(@subs.cues.map{ |c| c.end_time}).to eq correct_values
+      end
+
+      it "has correct duration" do
+        correct_values = [1084, 3254]
+        expect(@subs.cues.map{ |c| c.duration }).to eq correct_values
+      end
+
+      it "has correct text" do
+        correct_values = [
+          "Mr Developer, hi there.\n2004",
+          "You are changing the sample subtitle\nused for captions test",
+        ]
+        expect(@subs.cues.map{ |c| c.text }).to eq correct_values
+      end
+    end
   end
 end
